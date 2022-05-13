f8270f1c158e09f4ed1e6ae271d9ecbf335ec447
Sümeyye Almas <72462370+SumeyyeA@users.noreply.github.com>
2022-05-13 22:47:01 +0300

    Add files via upload

---
+++ NesneYonelimliAnalizTasarimProje/NesneYonelimliAnalizTasarimProje/Proje/src/sogutucuuygulamasi/SogutucuUygulamasi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sogutucuuygulamasi;
+
+/**
+ *
+ * @author Hp
+ */
+public class SogutucuUygulamasi {
+
+    /**
+     *
+     */
+    public static void main(String[] args) {
+        Kullanici1 k1 = new Kullanici1();
+        Kullanici2 k2 = new Kullanici2();
+
+        
+       Sogutucu basla = new Sogutucu();
+    }
+    
+}
